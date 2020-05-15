7261744c52d3f95dafb3fdb323628248ff991c70
Srajan  Rai <53820125+srajan95@users.noreply.github.com>
2020-05-15 19:07:26 +0100

    Update Linear Model.R

--- Linear Model.R
+++ Linear Model.R
@@ -1,4 +1,6 @@
+#Linear model is built
 model<-lm(sunday~daily,data=NewspaperData)
+#Summary of the model
 summary(model)
 test<-data.frame(NewspaperData$daily)
 test
@@ -6,12 +8,12 @@ pred<-predict(model,newdata = c(NewspaperData$daily))
 pred
 install.packages("ggplot2")
 library(ggplot2)
-
+#Various Plots 
 dotchart(NewspaperData$daily)
 boxplot(NewspaperData$daily)
 barplot(NewspaperData$daily)
 qplot(NewspaperData$daily,NewspaperData$sunday)
-
+# Predict the model
 pred<-predict(model)
 finaldata<-data.frame(NewspaperData,pred,"Error"=NewspaperData$sunday-pred)
 finaldata
